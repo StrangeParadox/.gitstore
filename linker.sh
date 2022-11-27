1a86a997e899bf9a312af2e256211d06056c9d50
StrangeParadox <d.ellis01.dje@gmail.com>
2022-11-27 03:33:16 +0100

    🚀 second commit!

--- linker.sh
+++ linker.sh
@@ -7,10 +7,38 @@ CONFIG_DIR="$HOME/.config"
 GITSTORE_DIR="$HOME/.gitstore"
 
 # Get all the files in the .config directory
-FILES=$(find $GITSTORE_DIR/.config -type f)
+ENTRIES=$(find $GITSTORE_DIR/.config)
 
 # Loop through all the files
-for FILE in $FILES; do
-    # Create a symlink to the file
-    ln -s $FILE $CONFIG_DIR/$(basename $FILE)
+for ENTRY in $ENTRIES; do
+    # Check if the entry is a file
+    if [ -f $ENTRY ]; then
+        # Check if the file already exists
+        if [ -f $CONFIG_DIR/$ENTRY ]; then
+            # Check if the file is a symlink
+            if [ -L $CONFIG_DIR/$ENTRY ]; then
+                # Remove the symlink
+                rm $CONFIG_DIR/$ENTRY
+            else
+                # Move the file to a backup directory
+                mkdir -p $GITSTORE_DIR/.config-backup
+                mv $CONFIG_DIR/$ENTRY $GITSTORE_DIR/.config-backup/$ENTRY
+            fi
+        fi
+    fi
+
+    # Check if the entry is a directory
+    if [ -d $ENTRY ]; then
+        # Remove the .config directory from the path
+        DIR=$(echo $ENTRY | sed "s|$GITSTORE_DIR/.config||g")
+
+        # Check if the directory exists
+        if [ ! -d $CONFIG_DIR$DIR ]; then
+            # Create the directory
+            mkdir -p $CONFIG_DIR$DIR
+        fi
+
+        # Print creating the directory
+        echo "Creating directory: $CONFIG_DIR$DIR"
+    fi
 done
